dc6ab3d670da0288553fbc17fa6acfd984f4e4fe
Chad Bramwell <chadbramwell@users.noreply.github.com>
2019-09-21 11:47:26 -0700

    fixing simplify code.

--- ++c/simplify.cpp
+++ ++c/simplify.cpp
@@ -25,9 +25,7 @@ ASTNode* simplify(const ASTNode* root, int* reductions)
         ++*reductions;
         return n;
     }
-
-    debug_break(); // TODO, fill out switch below
-
+    
     // deep copy while simplifying...
     ASTNode* n = new ASTNode(*root);
     switch (root->type)
@@ -42,16 +40,39 @@ ASTNode* simplify(const ASTNode* root, int* reductions)
             }
         } break;
         case AST_blocklist:
+        {
+            n->blocklist.nodes = NULL;
+            n->blocklist.size = 0;
+            for (uint32_t i = 0; i < root->blocklist.size; ++i)
+            {
+                astn_push(&n->blocklist, simplify(root->blocklist.nodes[i], reductions));
+            }
+        } break;
         case AST_ret: n->ret.expression = simplify(root->ret.expression, reductions); break;
         case AST_var:
-        case AST_num:
+        {
+            if (root->var.assign_expression)
+                n->var.assign_expression = simplify(root->var.assign_expression, reductions); break;
+        } break;
+        case AST_num: break;
         case AST_fdef:
-        case AST_fcall:
-        case AST_if:
-        case AST_for:
-        case AST_while:
-        case AST_dowhile:
+        {
+            n->fdef.body.nodes = NULL;
+            n->fdef.body.size = 0;
+            for (uint32_t i = 0; i < root->fdef.body.size; ++i)
+            {
+                astn_push(&n->fdef.body, simplify(root->fdef.body.nodes[i], reductions));
+            }
+        } break;
+        case AST_fcall:debug_break();
+        case AST_if:debug_break();
+        case AST_for:debug_break();
+        case AST_while:debug_break();
+        case AST_dowhile:debug_break();
         case AST_unop:
+        {
+            n->unop.on = simplify(root->unop.on, reductions);
+        } break;
         case AST_binop:
         case AST_terop:
         case AST_break:
